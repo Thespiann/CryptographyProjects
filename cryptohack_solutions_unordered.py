e1858166581edac50eeb9be553222134380a492a
Thespiann <100039436+Thespiann@users.noreply.github.com>
2023-12-26 13:49:08 +0200

    Create cryptohack_solutions_unordered.py

---
+++ cryptohack_solutions_unordered.py
@@ -0,0 +1,7 @@
+//Crypto On the web
+//Json Web Tokens
+//token appreciation
+import jwt
+encoded="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmbGFnIjoiY3J5cHRve2p3dF9jb250ZW50c19jYW5fYmVfZWFzaWx5X3ZpZXdlZH0iLCJ1c2VyIjoiQ3J5cHRvIE1jSGFjayIsImV4cCI6MjAwNTAzMzQ5M30.shKSmZfgGVvd2OSB2CGezzJ3N6WAULo3w9zCl_T47KQ"
+jwt.decode(encoded,options={"verify_signature":False})
+//{'flag': 'crypto{jwt_contents_can_be_easily_viewed}', 'user': 'Crypto McHack', 'exp': 2005033493}
