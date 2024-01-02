cc47b5f4490a59b329b47c17cd7720e5f51de37f
Thespiann <100039436+Thespiann@users.noreply.github.com>
2024-01-02 10:18:41 +0200

    Update cryptohack_solutions_unordered.py
    structure of aes solved

--- cryptohack_solutions_unordered.py
+++ cryptohack_solutions_unordered.py
@@ -67,7 +67,30 @@ square_root_mod_prime(mod(a, p))
 #-------------answer
 #crypto{bijection}
 
+
 #Resisting Bruteforce
 #-------------answer
 #crypto{biclique}
 
+#Structure of AES
+#-------------solution
+
+def matrix2bytes(matrix):
+    """ Converts a 4x4 matrix into a 16-byte array.  """
+    bytes_array = bytearray()
+    for i in matrix:
+        for j in i:
+            bytes_array.append(j)
+    return bytes_array
+  
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(matrix))
+
+#---------------output
+bytearray(b'crypto{inmatrix}')
