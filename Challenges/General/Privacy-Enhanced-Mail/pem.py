b304a391f576466be1fcc57bce0f927603db5bbe
Thespiann <100039436+Thespiann@users.noreply.github.com>
2024-01-14 13:32:06 +0200

    Delete Challenges directory

--- Challenges/General/Privacy-Enhanced-Mail/pem.py
+++
@@ -1,6 +0,0 @@
-from Crypto.PublicKey import RSA
-
-with open('privacy_enhanced_mail.pem','r') as key_file:
-    key = RSA.importKey(key_file.read())
-    private_key_d_decimal = key.d
-    print(private_key_d_decimal)
